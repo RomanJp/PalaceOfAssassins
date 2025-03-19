f0cfa1541564450dea997dcad197fcc1f73bd58c
RomanJp <44593908+RomanJp@users.noreply.github.com>
2025-03-19 10:02:17 +0900

    Update PalaceOfAssassins.lua

--- PalaceOfAssassins.lua
+++ PalaceOfAssassins.lua
@@ -1,20 +1,3 @@
 -- Saved Variables
 PalaceOfAssassinsDB = PalaceOfAssassinsDB or { hasViewedStory = false }
 
-local function LoadTopSectionTextures()
-    return
-    
-    if CardFrame1Texture then
-        CardFrame1Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture1")
-    end
-    if CardFrame2Texture then
-        CardFrame2Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture2")
-    end
-    if CardFrame3Texture then
-        CardFrame3Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture3")
-    end
-end
-
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", LoadTopSectionTextures)
