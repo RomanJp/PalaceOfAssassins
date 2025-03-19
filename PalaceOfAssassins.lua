000a9bfb53cad61c081c7e8a99a0beeb42e57fd6
RomanJp <44593908+RomanJp@users.noreply.github.com>
2025-03-19 10:33:25 +0900

    Update PalaceOfAssassins.lua

--- PalaceOfAssassins.lua
+++ PalaceOfAssassins.lua
@@ -1,15 +1,1 @@
-local function LoadTopSectionTextures()
-    if TopFrame1Texture then
-        TopFrame1Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\card.tga")
-    end
-    if TopFrame2Texture then
-        TopFrame2Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\card.tga")
-    end
-    if TopFrame3Texture then
-        TopFrame3Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\card.tga")
-    end
-end
 
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", LoadTopSectionTextures)
