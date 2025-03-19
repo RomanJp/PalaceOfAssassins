0e8b5eae1e6581d52e6b2cf60c19fd52130b7a09
RomanJp <44593908+RomanJp@users.noreply.github.com>
2025-03-19 10:12:20 +0900

    Update PalaceOfAssassins.lua

--- PalaceOfAssassins.lua
+++ PalaceOfAssassins.lua
@@ -1,3 +1,15 @@
--- Saved Variables
-PalaceOfAssassinsDB = PalaceOfAssassinsDB or { hasViewedStory = false }
+local function LoadTopSectionTextures()
+    if TopFrame1Texture then
+        TopFrame1Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\tooltip_border")
+    end
+    if TopFrame2Texture then
+        TopFrame2Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\tooltip_border")
+    end
+    if TopFrame3Texture then
+        TopFrame3Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\textures\\tooltip_border")
+    end
+end
 
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", LoadTopSectionTextures)
