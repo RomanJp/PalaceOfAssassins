a3a3e98d1eba4c65b694095befbeb4d4fcd9c236
RomanJp <44593908+RomanJp@users.noreply.github.com>
2025-03-19 09:49:49 +0900

    Update PalaceOfAssassins.lua

--- PalaceOfAssassins.lua
+++ PalaceOfAssassins.lua
@@ -1,2 +1,18 @@
 -- Saved Variables
-TheCodeOfHondzyoLawsDB = TheCodeOfHondzyoLawsDB or { hasViewedStory = false }
+PalaceOfAssassinsDB = PalaceOfAssassinsDB or { hasViewedStory = false }
+
+local function LoadTopSectionTextures()
+    if CardFrame1Texture then
+        CardFrame1Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture1")
+    end
+    if CardFrame2Texture then
+        CardFrame2Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture2")
+    end
+    if CardFrame3Texture then
+        CardFrame3Texture:SetTexture("Interface\\AddOns\\PalaceOfAssassins\\Textures\\Texture3")
+    end
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", LoadTopSectionTextures)
